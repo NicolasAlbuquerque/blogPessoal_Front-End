8ea384680234f36431d5a0cc08bef1a6c6d6b31d
NICOLAS ALBUQUERQUE <123500218+NicolasAlbuquerque@users.noreply.github.com>
2023-04-11 14:55:37 -0300

    adicionando imstalando do Material UI

---
+++ src/home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+
+
+
+
+function Home() {
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>      
+            <img src="https://avatars.githubusercontent.com/u/123500218?v=4" alt="imagem tela inicial" className='img'/>
+        </>
+    )
+}
+
+export default Home;
